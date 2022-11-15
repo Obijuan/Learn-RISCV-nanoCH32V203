14f3eebf35c1d05db45f33828479858f9e8c5356
Obijuan <obijuan.cube@gmail.com>
2022-11-15 12:23:16 +0100

    Hello: Minimal version

--- Examples/01-Ledon/main.c
+++ Examples/01-Ledon/main.c
@@ -1,52 +1,25 @@
 #include "ch32v20x.h"
 
-/* General Purpose I/O */
-typedef struct
-{
-    __IO uint32_t CFGLR;
-    __IO uint32_t CFGHR;
-    __IO uint32_t INDR;
-    __IO uint32_t OUTDR;
-    __IO uint32_t BSHR;
-    __IO uint32_t BCR;
-    __IO uint32_t LCKR;
-} GPIO_TypeDef;
-
-/* Reset and Clock Control */
-typedef struct
-{
-    __IO uint32_t CTLR;
-    __IO uint32_t CFGR0;
-    __IO uint32_t INTR;
-    __IO uint32_t APB2PRSTR;
-    __IO uint32_t APB1PRSTR;
-    __IO uint32_t AHBPCENR;
-    __IO uint32_t APB2PCENR;
-    __IO uint32_t APB1PCENR;
-    __IO uint32_t BDCTLR;
-    __IO uint32_t RSTSCKR;
-
-    __IO uint32_t AHBRSTR;
-    __IO uint32_t CFGR2;
-} RCC_TypeDef;
-
 /* Peripheral declaration */
-#define GPIOA   ((GPIO_TypeDef *)0x40010800)
-#define RCC     ((RCC_TypeDef *)0x40021000)
+#define GPIOA_CFGHR (0x40010800 + 1*4) 
+#define GPIOA_BSHR  (0x40010800 + 4*4)
+#define GPIOA_BCR   (0x40010800 + 5*4)
+#define RCC    (0x40021000 + 6*4) //-- APB2PCENR
 
 int main(void)
 {
     //-- Habilitar APB2: Puerto A
-    RCC->APB2PCENR = 0x4; 
+    *(uint32_t *)RCC = 0x4;
 
     //-- Configurar pin A15
-    GPIOA->CFGHR = 0x30000000;    
+    *(uint32_t *)GPIOA_CFGHR = 0x30000000;  
 
     //-- Turn LED on
-    GPIOA->BCR = 0x8000; 
+    *(uint32_t *)GPIOA_BCR = 0x8000;
 
     //-- Turn LED off
-    //GPIOA->BSHR = 0x8000;
+    //*(uint32_t *)GPIOA_BSHR = 0x8000;
+   
 
     while (1);
 }
