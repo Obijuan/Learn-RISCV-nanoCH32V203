597660c164538ec4dc5dffcce18b4b472664c8c1
Obijuan <obijuan.cube@gmail.com>
2022-11-15 10:03:33 +0100

    remove ch32v20x_gpio.h

--- Examples/01-Ledon/main.c
+++ Examples/01-Ledon/main.c
@@ -1,9 +1,42 @@
-#include "ch32v20x_gpio.h"
+#include "ch32v20x.h"
 #include "ch32v20x_rcc.h"
 
 #define My_RCC_APB2Periph_GPIOA           ((uint32_t)0x00000004)  
 #define My_GPIO_Pin_15                     ((uint16_t)0x8000) /* Pin 15 selected */
 
+/* Output Maximum frequency selection */
+typedef enum
+{
+    GPIO_Speed_10MHz = 1,
+    GPIO_Speed_2MHz,
+    GPIO_Speed_50MHz
+} GPIOSpeed_TypeDef;
+
+/* Configuration Mode enumeration */
+typedef enum
+{
+    GPIO_Mode_AIN = 0x0,
+    GPIO_Mode_IN_FLOATING = 0x04,
+    GPIO_Mode_IPD = 0x28,
+    GPIO_Mode_IPU = 0x48,
+    GPIO_Mode_Out_OD = 0x14,
+    GPIO_Mode_Out_PP = 0x10,
+    GPIO_Mode_AF_OD = 0x1C,
+    GPIO_Mode_AF_PP = 0x18
+} GPIOMode_TypeDef;
+
+/* GPIO Init structure definition */
+typedef struct
+{
+    uint16_t GPIO_Pin; /* Specifies the GPIO pins to be configured.
+                          This parameter can be any value of @ref GPIO_pins_define */
+
+    GPIOSpeed_TypeDef GPIO_Speed; /* Specifies the speed for the selected pins.
+                                     This parameter can be a value of @ref GPIOSpeed_TypeDef */
+
+    GPIOMode_TypeDef GPIO_Mode; /* Specifies the operating mode for the selected pins.
+                                   This parameter can be a value of @ref GPIOMode_TypeDef */
+} GPIO_InitTypeDef;
 
 /*********************************************************************
  * @fn      GPIO_Init
