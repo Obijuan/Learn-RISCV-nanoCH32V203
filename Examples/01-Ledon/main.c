75843498f89c8e58afc89e1fcb73bac9c8a7d1af
Obijuan <obijuan.cube@gmail.com>
2022-11-15 18:43:03 +0100

    Update Ledon

--- Examples/01-Ledon/main.c
+++ Examples/01-Ledon/main.c
@@ -4,12 +4,12 @@
 #define GPIOA_CFGHR (0x40010800 + 1*4) 
 #define GPIOA_BSHR  (0x40010800 + 4*4)
 #define GPIOA_BCR   (0x40010800 + 5*4)
-#define RCC    (0x40021000 + 6*4) //-- APB2PCENR
+#define RCC_APB2PCENR  (0x40021000 + 6*4)
 
 int main(void)
 {
     //-- Habilitar APB2: Puerto A
-    *(uint32_t *)RCC = 0x4;
+    *(uint32_t *)RCC_APB2PCENR = 0x4;
 
     //-- Configurar pin A15
     *(uint32_t *)GPIOA_CFGHR = 0x30000000;  
